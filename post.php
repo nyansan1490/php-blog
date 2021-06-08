9363d98e15863aa546113d58d5f93f716db5eba3
nyansan1490 <nyansan1490@gmail.com>
2021-06-08 17:09:43 +0900

    3-1 2.nav,secure切り出し

--- post.php
+++ post.php
@@ -1,5 +1,36 @@
 <?php
   include 'lib/secure.php';
+  include 'lib/connect.php';
+
+  $title = "";        // タイトル
+  $body = "";         // 本文
+  $title_alert = "";  // タイトルのエラー文言
+  $body_alert = "";   // 本文のエラー文言
+
+  if (!empty($_POST['title']) && !empty($_POST['body'])){
+    // titleとbodyがPOSTメソッドで送信されたとき
+    $title = $_POST['title'];
+    $body = $_POST['body'];
+    $db = new connect();
+    $sql = "INSERT INTO articles (title, body, created_at, updated_at)
+            VALUES (:title, :body, NOW(), NOW())";
+    $result = $db->query($sql, array(':title' => $title, ':body' => $body));
+    header('Location: backend.php');
+  } else if(!empty($_POST)){
+    // POSTメソッドで送信されたが、titleかbodyが足りないとき
+    // 存在するほうは変数へ、ない場合空文字にしてフォームのvalueに設定する
+    if (!empty($_POST['title'])){
+      $title = $_POST['title'];
+    } else {
+      $title_alert = "タイトルを入力してください。";
+    }
+
+    if (!empty($_POST['body'])){
+      $body = $_POST['body'];
+    } else {
+      $body_alert = "本文を入力してください。";
+    }
+  }
 ?>
 <!doctype html>
 <html lang="ja">
@@ -50,11 +81,13 @@
       <form action="post.php" method="post">
         <div class="mb-3">
           <label class="form-label">タイトル</label>
-          <input type="text" name="title" class="form-control">
+          <?php echo !empty($title_alert)? '<div class="alert alert-danger">'.$title_alert.'</div>': '' ?>
+          <input type="text" name="title" value="<?php echo $title; ?>" class="form-control">
         </div>
         <div class="mb-3">
           <label class="form-label">本文</label>
-          <textarea name="body" class="form-control" rows="10"></textarea>
+          <?php echo !empty($body_alert)? '<div class="alert alert-danger">'.$body_alert.'</div>': '' ?>
+          <textarea name="body" class="form-control" rows="10"><?php echo $body; ?></textarea>
         </div>
         <div class="mb-3">
           <button type="submit" class="btn btn-primary">投稿する</button>
