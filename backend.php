c9f8d81154a7a46e9ec96d9f608830d8c1a77e38
nyansan1490 <nyansan1490@gmail.com>
2021-06-09 10:26:54 +0900

    4-3 2.サムネイルの作成

--- backend.php
+++ backend.php
@@ -60,6 +60,7 @@
             <th>ID</th>
             <th>タイトル</th>
             <th>本文</th>
+            <th>画像</th>
             <th>作成日</th>
             <th>更新日</th>
             <th>編集</th>
@@ -71,6 +72,7 @@
             <td><?php echo $article->getId() ?></td>
             <td><?php echo $article->getTitle() ?></td>
             <td><?php echo $article->getBody() ?></td>
+            <td><?php echo $article->getFilename()? 'あり': 'なし' ?></td>
             <td><?php echo $article->getCreatedAt() ?></td>
             <td><?php echo $article->getUpdatedAt() ?></td>
             <td><a href="edit.php?id=<?php echo $article->getId() ?>" class="btn btn-success">編集</a></td>
@@ -83,6 +85,7 @@
         <p>記事はありません。</p>
       </div>
 <?php endif ?>
+
 
     </div>
 
