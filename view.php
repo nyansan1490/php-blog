d79c275c827e193e247d9d2fb4a912fafe85defc
nyansan1490 <nyansan1490@gmail.com>
2021-06-10 10:31:12 +0900

    6-1

--- view.php
+++ view.php
@@ -3,8 +3,14 @@
   include 'lib/queryArticle.php';
   include 'lib/article.php';
 
-  $queryArticle = new QueryArticle();
-  $articles = $queryArticle->findAll();
+  if (!empty($_GET['id'])){
+    $id = intval($_GET['id']);
+    
+    $queryArticle = new QueryArticle();
+    $article = $queryArticle->find($id);
+  } else {
+    $article = null;
+  }
 ?>
 <!doctype html>
 <html lang="ja">
@@ -50,18 +56,19 @@
   <div class="row">
     <div class="col-md-8">
 
-<?php if ($articles): ?>
-  <?php foreach ($articles as $article): ?>
+<?php if ($article): ?>
       <article class="blog-post">
-        <h2 class="blog-post-title">
-          <a href="view.php?id=<?php echo $article->getId() ?>">
-            <?php echo $article->getTitle() ?>
-          </a>
-        </h2>
+        <h2 class="blog-post-title"><?php echo $article->getTitle() ?></h2>
         <p class="blog-post-meta"><?php echo $article->getCreatedAt() ?></p>
         <?php echo nl2br($article->getBody()) ?>
+        <?php if ($article->getFilename()): ?>
+        <div>
+          <a href="/album/<?php echo $article->getFilename() ?>" target="_blank">
+            <img src="/album/thumbs-<?php echo $article->getFilename() ?>" class="img-fluid">
+          </a>
+        </div>
+        <?php endif ?>
       </article>
-  <?php endforeach ?>
 <?php else: ?>
       <div class="alert alert-succes">
         <p>記事はありません。</p>
@@ -72,12 +79,12 @@
 
     <div class="col-md-4">
       <div class="p-4 mb-3 bg-light rounded">
-        <h4>ブログについて</h4>
+        <h4 class="font-italic">ブログについて</h4>
         <p class="mb-0">毎日のなんてことない日常を書いていきます。</p>
       </div>
 
       <div class="p-4">
-        <h4>アーカイブ</h4>
+        <h4 class="font-italic">アーカイブ</h4>
         <ol class="list-unstyled mb-0">
           <li><a href="#">2021/06</a></li>
           <li><a href="#">2021/05</a></li>
